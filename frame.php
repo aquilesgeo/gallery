f43fd7c9ebcee80f002cf9de1761f38d6a81fef2
manuel.saucedo <manuel.saucedo@protecso.com.pe>
2017-06-01 18:38:31 -0500

    sample dir

---
+++ frame.php
@@ -0,0 +1,16 @@
+<?php
+    include "db.php";
+
+	$image = $_GET["i"];
+	$book = $_GET("b");
+	
+	$fileName = "$IMAGE_BASE/$book/$image";
+	if (file_exists($fileName)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');    
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
+
+    ?>
